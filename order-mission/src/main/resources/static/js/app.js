efd285dc3b4f36ce17526bf6ccb4f9e29873b89c
Arcas <darcas@emea.msad.sopra>
2017-03-09 08:27:01 +0100

    cosas arcas

--- order-mission/src/main/resources/static/js/app.js
+++ order-mission/src/main/resources/static/js/app.js
@@ -173,6 +173,30 @@ taskManagerModule.controller('collaCtrl', function ($scope, $http){
          });
      };
     
+     $scope.OpenData = function (x) {
+         // use $.param jQuery function to serialize data from JSON 
+          var data = ({
+         	collabFirstName : x.collabFirstName,
+ 				date : x.date,
+ 				project : x.project,
+ 				agency : x.agency,
+ 				division : x.division,
+ 				status : "Abierta"
+          });
+          
+          
+
+          $http.put('/collaboraters/' + x.id, data)
+          .success(function (data, status, headers, config) {
+              //$scope.PostDataResponse = data;
+          })
+          .error(function (data, status, header, config) {
+              $scope.ResponseDetails = "Data: " + data +
+                  "<hr />status: " + status +
+                  "<hr />headers: " + header +
+                  "<hr />config: " + config;
+          });
+      };
      
      $scope.reload = function()
      {
