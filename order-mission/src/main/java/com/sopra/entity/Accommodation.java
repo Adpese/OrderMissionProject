3926ae5b56da0cbcaacf6d5599ec505e2f719e34
Adrian <apelaez@emea.msad.sopra>
2017-03-13 12:47:09 +0100

    backend alojamiento

---
+++ order-mission/src/main/java/com/sopra/entity/Accommodation.java
@@ -0,0 +1,86 @@
+package com.sopra.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="accommodation")
+public class Accommodation {
+	
+	
+	@javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id_accommodation")
+    private int  Id;
+	 
+	 @Column(name="hotel_name")
+	 private String hotelName;
+	 @Column(name="entry_date")
+	 private String entryDate;
+	 @Column(name="departure_date")
+	 private String departureDate;
+	 @Column(name="number_of_nights")
+	 private String numberOfNights;
+	 @Column(name="price")
+	 private long price;
+	 @ManyToOne
+    @JoinColumn(name="id_mission")
+    private Mission mission;
+	
+	
+	 public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public String getEntryDate() {
+		return entryDate;
+	}
+	public void setEntryDate(String entryDate) {
+		this.entryDate = entryDate;
+	}
+	public String getDepartureDate() {
+		return departureDate;
+	}
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+	public String getNumberOfNights() {
+		return numberOfNights;
+	}
+	public void setNumberOfNights(String numberOfNights) {
+		this.numberOfNights = numberOfNights;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	public Mission getMission() {
+		return mission;
+	}
+	public void setMission(Mission mission) {
+		this.mission = mission;
+	}
+	
+	@Override
+	public String toString() {
+		return "Accommodation [Id=" + Id + ", hotelName=" + hotelName + ", entryDate=" + entryDate + ", departureDate=" + departureDate
+				+ ", numberOfNights=" + numberOfNights + "]";
+	}
+	 
+}
---
+++ order-mission/src/main/java/com/sopra/repository/AccommodationRepository.java
@@ -0,0 +1,9 @@
+package com.sopra.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sopra.entity.Accommodation;
+
+public interface AccommodationRepository extends CrudRepository<Accommodation, Integer> {
+
+}
