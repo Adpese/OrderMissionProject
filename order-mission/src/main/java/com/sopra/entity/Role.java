e262512d0c3421261aeea8896948b09b766e7123
GRANERO CIOCCHI Sebastian Daniel <sdgranero@emea.msad.sopra>
2017-03-23 08:37:26 +0100

    Sebas User, Role

--- order-mission/src/main/java/com/sopra/entity/Role.java
+++ order-mission/src/main/java/com/sopra/entity/Role.java
@@ -1,5 +1,38 @@
 package com.sopra.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Role")
 public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id_role")
+	private int Id;
+	@Column(name="role")
+	private String rol;
+
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getRol() {
+		return rol;
+	}
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
 
 }
--- order-mission/src/main/java/com/sopra/entity/User.java
+++ order-mission/src/main/java/com/sopra/entity/User.java
@@ -1,5 +1,49 @@
 package com.sopra.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name="User")
 public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id_user") 
+	private int Id;
+	@Column(name="name") 
+	private String Nombre;
+	@OneToOne
+	@JoinColumn(name="id_role")
+	private Role rol;
+	
+
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public Role getRol() {
+		return rol;
+	}
+	public void setRol(Role rol) {
+		this.rol = rol;
+	}
+	
+	
 
 }
